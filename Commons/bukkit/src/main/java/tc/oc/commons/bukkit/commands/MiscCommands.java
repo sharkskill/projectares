88b461f403489ddb0d001c4e2b5d79e99fb8aaa8
cswhite2000 <18whitechristop@gmail.com>
2018-01-25 00:42:21 -0800

    Add a command to list the locales players are using

--- Commons/bukkit/src/main/java/tc/oc/commons/bukkit/commands/MiscCommands.java
+++ Commons/bukkit/src/main/java/tc/oc/commons/bukkit/commands/MiscCommands.java
@@ -92,6 +92,36 @@ public class MiscCommands implements Commands {
     }
 
     @Command(
+            aliases = { "playerlocale", "locale" },
+            desc = "Shows statics on what locale players online are in",
+            flags = "a",
+            min = 0,
+            max = 1
+    )
+    @CommandPermissions("ocn.locale")
+    public void listPlayerLocales(final CommandContext args, final CommandSender sender) throws CommandException {
+        Audience audience = audiences.get(sender);
+        if (args.hasFlag('a')) {
+            Map<String, Integer> playerLocaleMap = new HashMap<>();
+            userStore.stream().forEach(player -> {
+                String locale = player.getLocale();
+                playerLocaleMap.put(locale, playerLocaleMap.getOrDefault(locale, 0) + 1);
+            });
+
+            audience.sendMessage(new HeaderComponent(new Component(ChatColor.AQUA).translate("list.player.locales.title")));
+            for (Map.Entry<String, Integer> entry : playerLocaleMap.entrySet()) {
+                audience.sendMessage(new TranslatableComponent("list.player.locales.message." + (entry.getValue() == 1 ? "singular" : "plural"),
+                        ChatColor.AQUA + entry.getValue().toString(),
+                        ChatColor.AQUA + entry.getKey(),
+                        String.format("%.1f", 100 * entry.getValue() / (double) userStore.count()) + "%"));
+            }
+        } else {
+            Player player = CommandUtils.getPlayerOrSelf(args, sender, 0);
+            audience.sendMessage(new TranslatableComponent("list.player.locale.singular.message", new PlayerComponent(identityProvider.createIdentity(player)), ChatColor.AQUA + player.getLocale()));
+        }
+    }
+
+    @Command(
             aliases = { "coinflip" },
             desc = "Flip a Coin",
             flags = "b",
