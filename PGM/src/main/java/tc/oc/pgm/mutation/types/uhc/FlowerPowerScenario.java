6ef32143b234e550aece5789700749ceeb16e818
ShinyDialga <shinydialga45@gmail.com>
2018-07-25 16:56:58 -0500

    Add banned items to flower power

--- PGM/src/main/java/tc/oc/pgm/mutation/types/uhc/FlowerPowerScenario.java
+++ PGM/src/main/java/tc/oc/pgm/mutation/types/uhc/FlowerPowerScenario.java
@@ -22,6 +22,7 @@ import java.util.Set;
 
 public class FlowerPowerScenario extends UHCMutation.Impl {
     private List<Material> flowers;
+    private List<Material> bannedItems;
 
     private static final List<Material> materials = Collections.unmodifiableList(Arrays.asList(Material.values()));
     static Range<Integer> SIZE;
@@ -35,7 +36,11 @@ public class FlowerPowerScenario extends UHCMutation.Impl {
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onItemSpawn(ItemSpawnEvent event) {
         if (flowers.contains(event.getEntity().getItemStack().getType())) {
-            event.getEntity().getItemStack().setType(materials.get(entropy().randomInt(SIZE)));
+            Material item = materials.get(entropy().randomInt(SIZE));
+            while (bannedItems.contains(item)) {
+                item = materials.get(entropy().randomInt(SIZE));
+            }
+            event.getEntity().getItemStack().setType(item);
         }
     }
 
@@ -43,6 +48,27 @@ public class FlowerPowerScenario extends UHCMutation.Impl {
         flowers = new ArrayList<>();
         flowers.add(Material.YELLOW_FLOWER);
         flowers.add(Material.RED_ROSE);
+
+        bannedItems = new ArrayList<>();
+        //too op
+        bannedItems.add(Material.TOTEM);
+
+        //too common
+        bannedItems.add(Material.BLACK_GLAZED_TERRACOTTA);
+        bannedItems.add(Material.BLUE_GLAZED_TERRACOTTA);
+        bannedItems.add(Material.BROWN_GLAZED_TERRACOTTA);
+        bannedItems.add(Material.CYAN_GLAZED_TERRACOTTA);
+        bannedItems.add(Material.GRAY_GLAZED_TERRACOTTA);
+        bannedItems.add(Material.GREEN_GLAZED_TERRACOTTA);
+        bannedItems.add(Material.LIGHT_BLUE_GLAZED_TERRACOTTA);
+        bannedItems.add(Material.LIME_GLAZED_TERRACOTTA);
+        bannedItems.add(Material.MAGENTA_GLAZED_TERRACOTTA);
+        bannedItems.add(Material.ORANGE_GLAZED_TERRACOTTA);
+        bannedItems.add(Material.PINK_GLAZED_TERRACOTTA);
+        bannedItems.add(Material.PURPLE_GLAZED_TERRACOTTA);
+        bannedItems.add(Material.RED_GLAZED_TERRACOTTA);
+        bannedItems.add(Material.SILVER_GLAZED_TERRACOTTA);
+        bannedItems.add(Material.YELLOW_GLAZED_TERRACOTTA);
     }
 
     @Override
